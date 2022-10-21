4cacb73441cfa923174acb38567dd7b6fb6a3eeb
Evgeniia <angela35@mail.ru>
2022-10-21 13:21:35 +0300

    Простые множители

--- Task_31.py
+++ Task_31.py
@@ -1,14 +1,14 @@
 # 31. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-number = int(input("Введите натуральное число N: "))
+number = int(input(f"Задайте натуральное число: "))
 simple_multipliers = []
-for i in range(number - 1, 1, -1):
-    simple = 0
-    if (number % i == 0):
-        for j in range(i - 1, 1, -1):
-            if (i % j == 0):
-                simple = simple + 1
-        if (simple == 0):
-            simple_multipliers.append(i)
-
+i = 2
+count = number
+while i <= count:
+    if count % i == 0:
+        simple_multipliers.append(i)
+        count //= i
+        i = 2
+    else:
+        i += 1
 print(f"Простые множители числа {number}: {simple_multipliers}")
