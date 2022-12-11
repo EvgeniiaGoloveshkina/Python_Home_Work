aa25191ebf8620a1df66144fa3dd12d9e406987d
Evgeniia <angela35@mail.ru>
2022-12-11 17:10:26 +0300

    Seminar_9

---
+++ Seminar_9/Weather_parsing.py
@@ -0,0 +1,23 @@
+# Прикрутить бота к парсингу валют или парсингу погоды в городе
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from pygismeteo import Gismeteo
+
+async def temperature(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    city = update.message.text
+    city = city.split(" ")
+    city = list(filter(lambda x: not "/temp" in x, city[1:]))
+    city = " ".join(city)
+    gismeteo = Gismeteo()
+    search_results = gismeteo.search.by_query(city)
+    city_id = search_results[0].id
+    current = gismeteo.current.by_id(city_id)
+    print(f"В городе {city} сейчас {current.temperature.air.c} *С")
+    await update.message.reply_text(f"В городе {city} сейчас {current.temperature.air.c} *С")
+
+
+app = ApplicationBuilder().token("5612298789:AAGwfSSMFliZLFvjPQXVbEV6XYGcIzYaX7w").build()
+app.add_handler(CommandHandler("temp", temperature))
+print("сервер запустился")
+app.run_polling()
